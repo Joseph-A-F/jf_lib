1e5fcd75641da83b78bb20c2259ea0a2738945e3
Joseph-A-F <149717113+Joseph-A-F@users.noreply.github.com>
2025-05-08 18:53:01 -0400

    initial commit

---
+++ .dir-locals.el
@@ -0,0 +1,4 @@
+;;; Directory Local Variables            -*- no-byte-compile: t -*-
+;;; For more information see (info "(emacs) Directory Variables")
+
+((dired-mode . ((cmake-ide-project-dir . ~/CLionProjects/jf_lib))))
---
+++ README.md
@@ -0,0 +1,3 @@
+# JF_lib
+
+A Pure-C library for the purpose of practicing implementing Data Structures and Algorithms, and to store helper functions for future use. 
